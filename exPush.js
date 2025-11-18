e7a9ab6f3cc7949a57c43351dbc3230018121ef7
JoaoMaster06 <jp848119@gmail.com>
2025-11-18 15:27:23 -0300

    Meus Jogos favoritos

---
+++ exPush.js
@@ -0,0 +1,9 @@
+let jogos = ["Resident Evil","Silksong"];
+
+//Adicionar em elemento no Final
+jogos.push("Zelda");
+console.log(jogos); //["Resident evil","Silksong","Zelda"]
+
+//Adicionar tudo de uma vez
+jogos.push("Gta 5","Red Dead redeption 2");
+console.log(jogos); 
