fd1b855afbc7d2ecb312dff290ecd33c8317ab79
JoaoMaster06 <jp848119@gmail.com>
2025-11-18 16:20:46 -0300

    Comida

---
+++ exPop.js
@@ -0,0 +1,6 @@
+let lanches = ["Pizza","Hamburguer","Macarrão","Picole"]
+
+//Remove o Último elemento 
+let removido = lanches.pop();
+console.log(lanches)//"Picole"
+console.log(lanches);//"Pizza","Hamburguer",Macarrão
