6ba041998b6c4b26f3f63e20465650745c52b337
JoaoMaster06 <jp848119@gmail.com>
2025-11-19 16:51:23 -0300

    Notas

---
+++ exfitro.js
@@ -0,0 +1,10 @@
+let notas = [4.5,8.8,6.5,9.0,3.0];
+
+//Versão completa com parênteses
+let aprovados = notas.filter((nota) => {
+    return nota >=7;
+});
+//Todas notas
+console.log(notas);
+//Aprovados
+console.log(aprovados)
